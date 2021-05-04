58a614f3b3129695ec12b7db3d048e2c41b5b1e2
Lee Martin <lee.martin@pliminus.co.uk>
2021-05-04 22:50:10 +0000

    Update: Breaking up into functions

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,28 @@ fn main() -> io::Result<()> {
         let lineres = line.unwrap();
         i = i + 1;
         match ImageReader::open(lineres)?.decode() {
-            Ok(okimg) => {
-                let savelocation = format!("{}/target/output/{}.jpg", env::current_dir().unwrap().to_string_lossy(), i);
-                match okimg.save(&savelocation) {
-                    Ok(_) => {
-                        match io::stdout().write((savelocation + "\n").as_bytes()) {
-                            Ok(_) => (),
-                            Err(_) => ()
-                        }
-                    },
-                    Err(_) => (println!("Error Save: {}", i))
-                }
-            },
-            Err(_) => (println!("Error read: {}", i))
+            Ok(okimg) => save_image_to_output(okimg, format!("{}", i)),
+            Err(_) => write_error_to_log(format!("Error read: {}", i))
         }
     }
     Ok(())
+}
+
+fn save_image_to_output(img: image::DynamicImage, filename: String) -> () {
+    let savelocation = format!("{}/target/output/{}.jpg", env::current_dir().unwrap().to_string_lossy(), filename);
+    match img.save(&savelocation) {
+        Ok(_) => write_path_to_stdout(savelocation),
+        Err(_) => write_error_to_log(format!("saving {}", filename))
+    }
+}
+
+fn write_path_to_stdout(savelocation: String) -> (){
+    match io::stdout().write((savelocation + "\n").as_bytes()) {
+        Ok(_) => (),
+        Err(_) => ()
+    }
+}
+
+fn write_error_to_log(error: String) -> (){
+    println!("Error: {}", error);
 }
