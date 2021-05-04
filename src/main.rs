12442c0e1aeb1c80ab060429e2b7be28738624ed
Lee Martin <lee.martin@pliminus.co.uk>
2021-05-04 22:07:06 +0000

    Update: Writing save locations to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Write };
 use std::env;
 use image::io::Reader as ImageReader;
 
@@ -7,12 +7,17 @@ fn main() -> io::Result<()> {
     let mut i = 0;
     for line in stdin.lock().lines() {
         let lineres = line.unwrap();
-        println!("{}", lineres);
         i = i + 1;
         match ImageReader::open(lineres)?.decode() {
             Ok(okimg) => {
-                match okimg.save(format!("{}/target/output/{}.png", env::current_dir().unwrap().to_string_lossy(), i)) {
-                    Ok(_) => (println!("Success: {}", i)),
+                let savelocation = format!("{}/target/output/{}.jpg", env::current_dir().unwrap().to_string_lossy(), i);
+                match okimg.save(&savelocation) {
+                    Ok(_) => {
+                        match io::stdout().write((savelocation + "\n").as_bytes()) {
+                            Ok(_) => (),
+                            Err(_) => ()
+                        }
+                    },
                     Err(_) => (println!("Error Save: {}", i))
                 }
             },
