8c233ab778ec218aa9f5c63c9410f4254408b720
Lee Martin <lee.martin@pliminus.co.uk>
2021-05-05 20:21:56 +0000

    Update: Bit of refactoring to try and not have massively indented code

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,38 @@ use image::io::Reader as ImageReader;
 fn main() -> io::Result<()> {
     let stdin = io::stdin();
     for stdline in stdin.lock().lines() {
-        match stdline {
-            Ok(line) => {
-                let mut parts = line.split(":");
-                if let Some(source) = parts.next() {
-                    if let Some(target) = parts.next() {
-                        match ImageReader::open(&source)?.decode() {
-                            Ok(okimg) => save_image_to_output(okimg, target.to_string()),
-                            Err(_) => write_error_to_log(format!("Error read: {}", source))
-                        }
-                    }
-                }
-            },
-            Err(_) => write_error_to_log("Failed to read stdin line".to_string())
-        }
-
+        process_line(stdline)
     }
     Ok(())
+}
+
+fn process_line(stdline: Result<std::string::String, std::io::Error>) -> () {
+    match stdline {
+        Ok(line) => turn_line_into_source_and_target(line.split(":")),
+        Err(_) => write_error_to_log("Failed to read stdin line".to_string())
+    }
+}
+
+fn turn_line_into_source_and_target(mut parts: std::str::Split<&str>) -> () {
+    if let Some(source) = parts.next() {
+        if let Some(target) = parts.next() {
+            return format_from_source_to_target(source, target)
+        }
+        return write_error_to_log("Failed to read target".to_string())
+    }
+    return write_error_to_log("Failed to read source".to_string())
+}
+
+fn format_from_source_to_target(source: &str, target: &str) -> () {
+    match ImageReader::open(&source) {
+        Ok(imgreader) =>{
+            match imgreader.decode() {
+                Ok(okimg) => save_image_to_output(okimg, target.to_string()),
+                Err(_) => write_error_to_log(format!("Error read: {}", source))
+            }
+        },
+        Err(_) => write_error_to_log(format!("Error read: {}", source))
+    }
 }
 
 fn save_image_to_output(img: image::DynamicImage, target: String) -> () {
@@ -30,13 +45,13 @@ fn save_image_to_output(img: image::DynamicImage, target: String) -> () {
     }
 }
 
-fn write_path_to_stdout(savelocation: String) -> (){
+fn write_path_to_stdout(savelocation: String) -> () {
     match io::stdout().write((savelocation + "\n").as_bytes()) {
         Ok(_) => (),
-        Err(_) => ()
+        Err(_) => write_error_to_log(format!("Failed to write to stdout"))
     }
 }
 
-fn write_error_to_log(error: String) -> (){
+fn write_error_to_log(error: String) -> () {
     println!("Error: {}", error);
 }
