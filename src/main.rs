34dd777d959b10c4d0206e5da57460bace5ebab9
Lee Martin <lee.martin@pliminus.co.uk>
2021-05-09 19:36:35 +0000

    Update: Using and_then to get rid of the hidden nesting madness

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,20 @@ struct ProcessingArguments {
     format: String
 }
 
+struct BulkImageSourceTarget {
+    source: String,
+    target: String
+}
+
+enum BulkImageFormatError {
+    SaveError,
+    StdOutError,
+    StdInError,
+    ReadFailure,
+    SourceError,
+    TargetError
+}
+
 fn main() -> io::Result<()> {
     let matches = App::new("BIF: Bulk Image Formatter")
                     .version("0.1.0")
@@ -31,53 +45,61 @@ fn main() -> io::Result<()> {
 }
 
 fn process_line(stdline: Result<std::string::String, std::io::Error>, arguments: &ProcessingArguments) -> () {
-    match stdline {
-        Ok(line) => turn_line_into_source_and_target(line.split(":"), arguments),
-        Err(_) => write_error_to_log("Failed to read stdin line".to_string())
-    }
+    if let Ok(bulksourcetarget) = stdline.map_err(|e| BulkImageFormatError::StdInError)
+        .and_then(|line| turn_line_into_source_and_target(line.split(":"))) {
+
+            let _result = format_from_source_to_target(bulksourcetarget.source.as_str())
+                .and_then(|img| save_image_to_output(img, bulksourcetarget.target, arguments))
+                .and_then(|saveloc| write_path_to_stdout(saveloc));
+
+        }
+    
+    return ();
 }
 
-fn turn_line_into_source_and_target(mut parts: std::str::Split<&str>, arguments: &ProcessingArguments) -> () {
+fn turn_line_into_source_and_target(mut parts: std::str::Split<&str>) -> Result<BulkImageSourceTarget, BulkImageFormatError> {
     if let Some(source) = parts.next() {
         if let Some(target) = parts.next() {
-            return format_from_source_to_target(source, target, arguments)
+            return Ok(BulkImageSourceTarget{source: source.to_string(), target: target.to_string()});
         }
-        return write_error_to_log("Failed to read target".to_string())
+        write_error_to_log("Failed to read target".to_string());
+        return Err(BulkImageFormatError::TargetError);
     }
-    return write_error_to_log("Failed to read source".to_string())
+    write_error_to_log("Failed to read source".to_string());
+    return Err(BulkImageFormatError::SourceError);
 }
 
-fn format_from_source_to_target(source: &str, target: &str, arguments: &ProcessingArguments) -> () {
-    match ImageReader::open(&source) {
-        Ok(imgreader) =>{
-            match imgreader.decode() {
-                Ok(okimg) => save_image_to_output(okimg, target.to_string(), arguments),
-                Err(_) => write_error_to_log(format!("Error read: {}", source))
-            }
-        },
-        Err(_) => write_error_to_log(format!("Error read: {}", source))
+fn format_from_source_to_target(source: &str) -> Result<image::DynamicImage, BulkImageFormatError> {
+    if let Ok(imgreader) = ImageReader::open(&source) {
+        if let Ok(okimg) = imgreader.decode() {
+            return Ok(okimg);
+        }
+        
     }
+    write_error_to_log(format!("Error read: {}", source));
+    Err(BulkImageFormatError::ReadFailure)
 }
 
-fn save_image_to_output(img: image::DynamicImage, target: String, arguments: &ProcessingArguments) -> () {
+fn save_image_to_output(img: image::DynamicImage, target: String, arguments: &ProcessingArguments) -> Result<String, BulkImageFormatError>  {
     
     let path_to_target = Path::new(&target);
     let path_to_target = path_to_target.with_extension(&arguments.format);
 
     if let Some(path) = path_to_target.as_path().to_str() {
-        match img.save(&path) {
-            Ok(_) => write_path_to_stdout(path.to_string()),
-            Err(_) => write_error_to_log(format!("saving {}", target))
+        if let Ok(_) = img.save(&path) {
+            return Ok(path.to_string());
         }
     }
-
+    write_error_to_log(format!("Failed to write to save {}", target));
+    Err(BulkImageFormatError::SaveError)
 }
 
-fn write_path_to_stdout(savelocation: String) -> () {
-    match io::stdout().write((savelocation + "\n").as_bytes()) {
-        Ok(_) => (),
-        Err(_) => write_error_to_log(format!("Failed to write to stdout"))
+fn write_path_to_stdout(savelocation: String) -> Result<(), BulkImageFormatError> {
+    if let Ok(_) = io::stdout().write((savelocation + "\n").as_bytes()) {
+        return Ok(())
     }
+    write_error_to_log(format!("Failed to write to stdout"));
+    Err(BulkImageFormatError::StdOutError)
 }
 
 fn write_error_to_log(error: String) -> () {
